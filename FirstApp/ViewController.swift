47fe4331dc797238b26935d97e7cb9e677c5d5db
Zach Byers <byerszach@gmail.com>
2018-01-28 19:51:35 -0700

    console log testing

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    let thing = "Billy Bob"
+    print("Hello \(thing)")
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 
 
 }
